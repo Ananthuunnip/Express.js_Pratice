d637ab8fd8655486d46d78d9b3c5c4b1bbe5122a
Ananthuunnip <ananthu2929@gmail.com>
2024-03-20 15:31:41 -0700

    added the middleware

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express = require("express");
 
 const app = express();
 
+app.use((req,res,next)=>{
+    console.log("HI I Am Medilware Here")
+    next();   // this allows to continue to  the another middlewrae
+})
+app.use((req,res,next)=>{
+    console.log("HI I Am another Medilware Here")
+    res.send('<h1>hello fucking Express</h1>')
+})
 
 const server = http.createServer(app);
 server.listen(4000)
