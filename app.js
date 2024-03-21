75d59a9d31f0c7d5b8dec672b991eaf9328e2345
Ananthuunnip <ananthu2929@gmail.com>
2024-03-21 13:28:55 -0700

    add new input tage name size

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product',(req,res,next)=>{
-    res.send('<form action ="/product" method="POST"><labell>Enter product</label><br><input type="text" name="Title"><br><button type="submit">Submit</button>')
+    res.send(
+        `<form action ="/product" method="POST">
+        <labell>Enter product  :  </label><input type="text" name="Title"><br>
+        <labell>Enter Size of product   :   </label><input type="text" name="size">
+        <button type="submit">Submit</button>`)
 })
-
 app.use('/product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/')
