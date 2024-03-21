e9a13d3a53b9c12f577424281a207854ba77a851
Ananthuunnip <ananthu2929@gmail.com>
2024-03-21 15:24:35 -0700

    add the filter

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const shopeRoutes = require('./routes/shope');   //module importing from admin
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
-app.use(shopeRoutes);
+app.use('/admin',adminRoutes);
+app.use('/shope',shopeRoutes);
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Page Not Found</h1>')
 })
