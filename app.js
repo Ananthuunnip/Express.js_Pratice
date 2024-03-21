db7985857420990d9e908b23b6639dab5774aee5
Ananthuunnip <ananthu2929@gmail.com>
2024-03-21 14:20:25 -0700

    add routers and make structure to app.js

--- app.js
+++ app.js
@@ -2,23 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send(
-        `<form action ="/product" method="POST">
-        <labell>Enter product  :  </label><input type="text" name="Title"><br>
-        <labell>Enter Size of product   :   </label><input type="text" name="size">
-        <button type="submit">Submit</button>`)
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
+const adminRoutes = require('./routes/admin');    //module importing from admin
+const shopeRoutes = require('./routes/shope');   //module importing from admin
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello this is Express</h1>')
-})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(adminRoutes);
+app.use(shopeRoutes);
+
 app.listen(4000)
 
 
